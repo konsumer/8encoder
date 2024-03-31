628f8465c59ec22985240472a775ef1879e5d3a3
David Konsumer <konsumer@jetboystudio.com>
2024-03-30 18:49:45 -0700

    fleshing out more of wire lib

--- wiringpi-8encoder/test.c
+++ wiringpi-8encoder/test.c
@@ -2,6 +2,63 @@
 #include "wiringpi-8encoder.h"
 
 int main(int argc, char* argv[]) {
-  int fd = wiringPiI2CSetup(ENCODER_ADDR);
-  printf("switch: %s\n", wiringpi_8encoder_getSwitchStatus(fd) ? "up" : "down");
+  int fd;
+
+  if ((fd = wiringPiI2CSetup(ENCODER_ADDR)) < 0) {
+    printf("I2CSetup Failed, %i\n", fd);
+    return 1;
+  }
+
+  printf("Firmware version: %d\nAddress: 0x%.2x\n", wiringpi_8encoder_get_firmware_version(fd), wiringpi_8encoder_get_address(fd));
+
+  wiringpi_8encoder_set_led_color_int(fd, 0, 0xff0000);
+  wiringpi_8encoder_set_led_color_int(fd, 1, 0x00ff00);
+  wiringpi_8encoder_set_led_color_int(fd, 2, 0x0000ff);
+
+  int i;
+
+  bool b[8] = {false};
+  bool bo[8] = {false};
+  uint8_t r[8] = {false};
+  uint8_t ro[8] = {false};
+  bool s = false;
+  bool os = false;
+
+  // set/display initial
+  s = wiringpi_8encoder_get_switch_value(fd);
+  printf("Switch position: %d\n", s);
+  os = s;
+  for (i = 0; i < 8; i++) {
+    b[i] = wiringpi_8encoder_is_button_down(fd, i);
+    printf("Button %d %s\n", i, b[i] ? "PUSHED" : "NO");
+    bo[i] = b[i];
+    r[i] = wiringpi_8encoder_get_encoder_value(fd, i);
+    printf("Rotary %d %d\n", i, r[i]);
+    ro[i] = r[i];
+  }
+
+  // fire events on change
+  while (1) {
+    s = wiringpi_8encoder_get_switch_value(fd);
+    if (s != os) {
+      printf("Switch position: %d\n", s);
+    }
+    os = s;
+
+    for (i = 0; i < 8; i++) {
+      b[i] = wiringpi_8encoder_is_button_down(fd, i);
+      if (b[i] != bo[i]) {
+        printf("Button %d %s\n", i, b[i] ? "PUSHED" : "NO");
+      }
+      bo[i] = b[i];
+
+      r[i] = wiringpi_8encoder_get_encoder_value(fd, i);
+      if (r[i] != ro[i]) {
+        printf("Rotary %d %d\n", i, r[i]);
+      }
+      ro[i] = r[i];
+    }
+  }
+
+  return 0;
 }
--- wiringpi-8encoder/wiringpi-8encoder.h
+++ wiringpi-8encoder/wiringpi-8encoder.h
@@ -17,26 +17,64 @@
 #define FIRMWARE_VERSION_REG 0xFE
 #define I2C_ADDRESS_REG 0xFF
 
-uint32_t wiringpi_8encoder_getEncoderValue(int fd, uint8_t index) {}
-
-void wiringpi_8encoder_setEncoderValue(int fd, uint8_t index, int32_t value) {}
-
-uint32_t wiringpi_8encoder_getIncrementValue(int fd, uint8_t index) {}
-
-bool wiringpi_8encoder_getButtonStatus(int fd, uint8_t index) {}
-
-bool wiringpi_8encoder_getSwitchStatus(int fd) {
-  return wiringPiI2CReadReg8(fd, SWITCH_REG);
+// Check to see if a rotary pushbutton (0-7) is pressed
+bool wiringpi_8encoder_is_button_down(uint8_t fd, uint8_t index) {
+  wiringPiI2CWrite(fd, BUTTON_REG + index);
+  int i = wiringPiI2CRead(fd);
+  return i != 1;
 }
 
-void wiringpi_8encoder_setLEDColor(int fd, uint8_t index, uint32_t color) {}
+// Get the current value of an encoder (0-7)
+int wiringpi_8encoder_get_encoder_value(uint8_t fd, uint8_t index) {
+  wiringPiI2CWrite(fd, ENCODER_REG + (index * 4));
+  return wiringPiI2CRead(fd);
+}
 
-void wiringpi_8encoder_setAllLEDColor(int fd, uint32_t color) {}
+// Set the current value of an encoder (0-7)
+void wiringpi_8encoder_set_encoder_value(uint8_t fd, uint8_t index, int32_t value) {}
 
-uint8_t wiringpi_8encoder_setI2CAddress(int fd, uint8_t addr) {}
+// Get the increment-value for an encoder (0-7)
+// TODO: not sure if this really works right
+uint32_t wiringpi_8encoder_get_increment_value(uint8_t fd, uint8_t index) {
+  wiringPiI2CWrite(fd, INCREMENT_REG + (index * 4));
+  return wiringPiI2CRead(fd);
+}
 
-uint8_t wiringpi_8encoder_getI2CAddress(int fd) {}
+// Get current position of toggle switch
+uint8_t wiringpi_8encoder_get_switch_value(uint8_t fd) {
+  wiringPiI2CWrite(fd, SWITCH_REG);
+  return wiringPiI2CRead(fd);
+}
 
-uint8_t wiringpi_8encoder_getFirmwareVersion(int fd) {}
+// Set the RGB LED (0-7) color using a single int like 0xFF0000
+void wiringpi_8encoder_set_led_color_int(uint8_t fd, uint8_t index, uint32_t color) {
+  uint8_t red = (color >> 16) & 0xff;
+  uint8_t green = (color >> 8) & 0xff;
+  uint8_t blue = color & 0xff;
+  printf("RGB %d: (%d, %d, %d)\n", index, red, green, blue);
 
-void wiringpi_8encoder_resetCounter(int fd, uint8_t index) {}
+  wiringPiI2CWriteReg8(fd, (index * 3) + RGB_LED_REG, red);
+  wiringPiI2CWriteReg8(fd, (index * 3) + RGB_LED_REG + 1, green);
+  wiringPiI2CWriteReg8(fd, (index * 3) + RGB_LED_REG + 2, blue);
+}
+
+// Set the RGB LED (0-7) color using seperate 0-1 floats for H, S, V
+void wiringpi_8encoder_set_led_color_hsv(uint8_t fd, uint8_t index, float h, float s, float v) {}
+
+// Get the firmware-version, on the controller
+uint8_t wiringpi_8encoder_get_firmware_version(uint8_t fd) {
+  wiringPiI2CWrite(fd, FIRMWARE_VERSION_REG);
+  return wiringPiI2CRead(fd);
+}
+
+// Set the current i2c address for device
+uint8_t wiringpi_8encoder_set_address(uint8_t fd, uint8_t addr) {}
+
+// Get the current i2c address from device
+uint8_t wiringpi_8encoder_get_address(uint8_t fd) {
+  wiringPiI2CWrite(fd, I2C_ADDRESS_REG);
+  return wiringPiI2CRead(fd);
+}
+
+// Reset a counter (by index: 0-7)
+void wiringpi_8encoder_reset_counter(uint8_t fd, uint8_t index) {}
