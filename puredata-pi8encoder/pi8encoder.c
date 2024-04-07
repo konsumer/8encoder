c328ed9e14857848ebad3ee10015cbafcdb2c833
David Konsumer <konsumer@jetboystudio.com>
2024-04-06 20:43:27 -0700

    fix audio by only reading half of inputs

--- puredata-pi8encoder/pi8encoder.c
+++ puredata-pi8encoder/pi8encoder.c
@@ -8,36 +8,43 @@ typedef struct _pi8encoder {
   t_object x_obj;
   t_outlet* x_out;
   int i2c;
+  int tracking_buttons[8];
+  int tracking_rotary[8];
+  int tracking_switch;
+  bool current_pass;
 } t_pi8encoder;
 
-static void pi8encoder_free(t_pi8encoder* x) {
-  post("[pi8encoder] terminated");
-}
-
-static void* pi8encoder_new() {
-  t_pi8encoder* x = (t_pi8encoder*)pd_new(pi8encoder_class);
-  x->x_out = outlet_new(&x->x_obj, gensym("list"));
-  x->i2c = open("/dev/i2c-1", O_RDWR);
-  if (x->i2c < 0 || ioctl(x->i2c, I2C_SLAVE, ENCODER_ADDR) < 0) {
-    post("[pi8encoder] init error.");
-    // TODO: how to return error here, so it fails?
-    return NULL;
-  }
-  post("[pi8encoder] initialised.");
-  return x;
-}
-
-static void pi8encoder_bang(t_pi8encoder* x) {
-  t_atom msg[17] = {0};
+static void pi8encoder_outputchanges(t_pi8encoder* x) {
+  t_atom msg[3] = {};
+  x->current_pass = !x->current_pass;
   int v = linux_8encoder_switch(x->i2c);
-  SETFLOAT(&msg[16], v);
-  for (int i = 0; i < 8; i++) {
-    v = linux_8encoder_get_counter(x->i2c, i);
-    SETFLOAT(&msg[i], v);
-    v = linux_8encoder_button_down(x->i2c, i) ? 1 : 0;
-    SETFLOAT(&msg[i + 8], v);
+  if (x->tracking_switch != v) {
+    SETSYMBOL(&msg[0], gensym("switch"));
+    SETFLOAT(&msg[1], v);
+    outlet_list(x->x_out, 0, 2, msg);
+    x->tracking_switch = v;
   }
-  outlet_list(x->x_out, 0, 17, msg);
+  for (int i = 0; i < 8; i++) {
+    if (x->current_pass) {
+      v = linux_8encoder_get_counter(x->i2c, i);
+      if (x->tracking_rotary[i] != v) {
+        SETSYMBOL(&msg[0], gensym("rotary"));
+        SETFLOAT(&msg[1], i);
+        SETFLOAT(&msg[2], v);
+        outlet_list(x->x_out, 0, 3, msg);
+        x->tracking_rotary[i] = v;
+      }
+    } else {
+      v = linux_8encoder_button_down(x->i2c, i) ? 1 : 0;
+      if (x->tracking_buttons[i] != v) {
+        SETSYMBOL(&msg[0], gensym("button"));
+        SETFLOAT(&msg[1], i);
+        SETFLOAT(&msg[2], v);
+        outlet_list(x->x_out, 0, 3, msg);
+        x->tracking_buttons[i] = v;
+      }
+    }
+  }
 }
 
 static void pi8encoder_handle_rgb(t_pi8encoder* x, t_floatarg i, t_floatarg r, t_floatarg g, t_floatarg b) {
@@ -58,6 +65,29 @@ static void pi8encoder_handle_hsv(t_pi8encoder* x, t_floatarg i, t_floatarg h, t
 
 static void pi8encoder_handle_rotary(t_pi8encoder* x, t_floatarg i, t_floatarg value) {
   linux_8encoder_set_counter(x->i2c, i, value);
+}
+
+static void pi8encoder_free(t_pi8encoder* x) {
+  post("[pi8encoder] terminated");
+}
+
+static void* pi8encoder_new() {
+  t_pi8encoder* x = (t_pi8encoder*)pd_new(pi8encoder_class);
+  x->i2c = open("/dev/i2c-1", O_RDWR);
+  if (x->i2c < 0 || ioctl(x->i2c, I2C_SLAVE, ENCODER_ADDR) < 0) {
+    post("[pi8encoder] init error.");
+    // TODO: how to return error here, so it fails?
+    return NULL;
+  }
+  x->x_out = outlet_new(&x->x_obj, gensym("list"));
+  for (int i = 0; i < 8; i++) {
+    x->tracking_buttons[i] = 0;
+    x->tracking_rotary[i] = 0;
+  }
+  x->tracking_switch = 0;
+
+  post("[pi8encoder] initialised.");
+  return x;
 }
 
 void pi8encoder_setup(void) {
@@ -94,5 +124,5 @@ void pi8encoder_setup(void) {
       A_DEFFLOAT,
       0);
 
-  class_addbang(pi8encoder_class, pi8encoder_bang);
+  class_addbang(pi8encoder_class, pi8encoder_outputchanges);
 }
